96c72629af3666d7724717a85673f4ba2f70220b
Lohit Talasila <Lohit9@users.noreply.github.com>
2015-09-24 16:45:22 -0400

    updated files

---
+++ s3-objectiveC/s3-objectiveC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  s3-objectiveC
+//
+//  Created by Lohit Talasila
+
+
+#import <UIKit/UIKit.h>
+
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ s3-objectiveC/s3-objectiveC/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  s3-objectiveC
+//
+//  Created by Lohit Talasila 
+
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
